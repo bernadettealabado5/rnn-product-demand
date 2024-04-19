d8d892edade98badbb13d0969dbc2f908e471eb1
louiecerv <lfcervantes@wvsu.edu.ph>
2024-04-20 05:54:39 +0800

    Save

--- Demand_Forecasting.py
+++ Demand_Forecasting.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import matplotlib.pyplot as plt
 
 def app():
     st.title('Demand Forecasting')
@@ -22,6 +23,38 @@ def app():
     st.write("\nFiltered data:")
     st.write(filtered_df)
     st.write(filtered_df.shape)
+    df1 = filtered_df.copy()
+    df1 = df1.loc[:, ['date', 'sales']]
+    df1['date'] = pd.to_datetime(df1['date'])
+
+    # Set the 'date' column as the index
+    df1.set_index('date', inplace=True)
+
+    st.write("The TIme Series Dataset")
+    st.write(df1)   
+    st.erite(df1.shape)
+    
+    st.write("The Time Series Plot")
+
+    # Assuming your dataframe is called 'df'
+    fig, ax = plt.subplots()  # Create a figure and an axes
+
+    # Plot the timeseries data on the axes
+    ax.plot(df1['sales'])
+
+    # Optional customizations
+    ax.set_title('Sales Over Time')
+    ax.set_xlabel('Date')
+    ax.set_ylabel('Sales')
+    ax.grid(True)  # Add gridlines for better readability
+
+    # Limit the number of ticks on the x-axis to 10 (adjust as needed)
+    plt.xticks(rotation=45)  # Rotate x-axis labels for better readability (optional)
+    locator = plt.MaxNLocator(nbins=10)
+    ax.xaxis.set_major_locator(locator)
+    st.pyplot(fig)
+
+
 
 if __name__ == "__main__":
     app()   
